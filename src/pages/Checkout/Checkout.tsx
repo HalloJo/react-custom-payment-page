3ade493d9e83e2355d2fad750e05bbe0ee01d116
Jorik van Ruiswijk <jorikvanruiswijk@gmail.com>
2024-07-11 15:12:36 +0200

    Finetuning desktop query

--- src/pages/Checkout/Checkout.tsx
+++ src/pages/Checkout/Checkout.tsx
@@ -75,74 +75,76 @@ const Checkout = () => {
 
   return (
     <div className="checkout">
-      <img src={expertsLogo} alt="Experts logo" className="checkout__logo" />
-      <h1 className="checkout__title">{CheckoutPageContent.title}</h1>
-      <p className="checkout__subTitle">{CheckoutPageContent.subTitle}</p>
-      <ul className="checkout__list">
-        {availablePaymentMethods.map((method) => (
-          <div
-            key={method.id}
-            className={`checkout__method ${
-              selectedMethod === method.id ? "selected" : ""
-            }`}
-          >
-            <div className="checkout__method_item">
-              <div className="checkout__method_image">
-                <img src={method.image} alt={method.description} />
-                <label htmlFor={method.id}>{method.description}</label>
-              </div>
-              <div className="checkout__radio">
-                {method.popular && (
-                  <span className="checkout__popular">
-                    {CheckoutPageContent.popular}
-                  </span>
-                )}
-                <input
-                  type="radio"
-                  id={method.id}
-                  name="paymentMethod"
-                  checked={selectedMethod === method.id}
-                  onChange={() => handleMethodSelection(method.id)}
-                />
-              </div>
-            </div>
-            {selectedMethod === method.id &&
-              method.issuers &&
-              method.issuers.length > 0 && (
-                <div className="checkout__issuers">
-                  <label htmlFor="issuer">Select Bank:</label>
-                  <select
-                    id="issuer"
-                    onChange={(e) => setSelectedIssuer(e.target.value)}
-                  >
-                    <option value="">Select an issuer</option>
-                    {method.issuers.map((issuer) => (
-                      <option key={issuer.id} value={issuer.name}>
-                        {issuer.name}
-                      </option>
-                    ))}
-                  </select>
+      <div className="checkout__wrapper">
+        <img src={expertsLogo} alt="Experts logo" className="checkout__logo" />
+        <h1 className="checkout__title">{CheckoutPageContent.title}</h1>
+        <p className="checkout__subTitle">{CheckoutPageContent.subTitle}</p>
+        <ul className="checkout__list">
+          {availablePaymentMethods.map((method) => (
+            <div
+              key={method.id}
+              className={`checkout__method ${
+                selectedMethod === method.id ? "selected" : ""
+              }`}
+            >
+              <div className="checkout__method_item">
+                <div className="checkout__method_image">
+                  <img src={method.image} alt={method.description} />
+                  <label htmlFor={method.id}>{method.description}</label>
                 </div>
-              )}
-          </div>
-        ))}
-        {showContinueButton && selectedPaymentMethod && (
-          <Link to={"/completed"} className="checkout__button">
-            Betalen met{" "}
-            <img
-              src={selectedPaymentMethod.image}
-              alt={selectedPaymentMethod.description}
-            />
-          </Link>
-        )}
-      </ul>
-      <div className="checkout__message">
-        <img
-          src={shield}
-          alt="Experts logo"
-          className="checkout__message_icon"
-        />
-        <p>{CheckoutPageContent.message}</p>
+                <div className="checkout__radio">
+                  {method.popular && (
+                    <span className="checkout__popular">
+                      {CheckoutPageContent.popular}
+                    </span>
+                  )}
+                  <input
+                    type="radio"
+                    id={method.id}
+                    name="paymentMethod"
+                    checked={selectedMethod === method.id}
+                    onChange={() => handleMethodSelection(method.id)}
+                  />
+                </div>
+              </div>
+              {selectedMethod === method.id &&
+                method.issuers &&
+                method.issuers.length > 0 && (
+                  <div className="checkout__issuers">
+                    <label htmlFor="issuer">Select Bank:</label>
+                    <select
+                      id="issuer"
+                      onChange={(e) => setSelectedIssuer(e.target.value)}
+                    >
+                      <option value="">Select an issuer</option>
+                      {method.issuers.map((issuer) => (
+                        <option key={issuer.id} value={issuer.name}>
+                          {issuer.name}
+                        </option>
+                      ))}
+                    </select>
+                  </div>
+                )}
+            </div>
+          ))}
+          {showContinueButton && selectedPaymentMethod && (
+            <Link to={"/completed"} className="checkout__button">
+              Betalen met{" "}
+              <img
+                src={selectedPaymentMethod.image}
+                alt={selectedPaymentMethod.description}
+              />
+            </Link>
+          )}
+        </ul>
+        <div className="checkout__message">
+          <img
+            src={shield}
+            alt="Experts logo"
+            className="checkout__message_icon"
+          />
+          <p>{CheckoutPageContent.message}</p>
+        </div>
       </div>
     </div>
   );
